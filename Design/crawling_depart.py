27d0be4eead36896b4189f57e960230258a6a76d
KIMTHE <kcms2369@naver.com>
2020-12-15 18:53:08 +0900

    각 알림을 쓰레드로 구현하는중- 미완성

--- Design/crawling_depart.py
+++ Design/crawling_depart.py
@@ -1,4 +1,5 @@
-import requests, time
+import requests, time, threading
+from win10toast import ToastNotifier
 from bs4 import BeautifulSoup
 
 from setting_depart import *
@@ -6,7 +7,10 @@ from setting_depart import *
 URL = 'https://computer.knu.ac.kr/06_sub/02_sub.html' #여기에 다른학부 URL도 추가가능
 
 class depart_noti :
-    #def __init__(self) :
+    def __init__(self,depart) :
+        self.depart = depart
+        self.noti = ''
+        self.send_thread_list = [] #각 알림을 쓰레드를 사용하고, 쓰레드들의 리스트
 
     def get_change(self):
         req = requests.get(URL)
@@ -37,7 +41,7 @@ class depart_noti :
             if(update_check==0): #ON/OFF에서 OFF시 update_check = 0으로 할예정
                 break
 
-            time.sleep(60)
+            time.sleep(2)
             print('새로고침')
 
             req = requests.get(URL)
@@ -64,14 +68,24 @@ class depart_noti :
             if(num_max == pre_max+1):        #변경사항 있을때, 키워드 체크
                 check_keyword(self,title)
                 
-            else:                           #변경사항 없을때, 아무것도 안함
-                print("변경사항없음")
+            else:                           #변경사항 없을때, 아무것도 안함, 밑에 출력은 테스트용
+                self.noti = "변경사항없음"
+                send_thread = threading.Thread(target = self.send_noti, args = (self.depart, self.noti))
+                self.send_thread_list.append(send_thread)
+                send_thread.start()
+
+                print(self.noti)
     
     def check_keyword(self,title):
         setting.load()
 
         if not setting.keyword:   #키워드리스트 설정안했을때
-            print("변경사항있음 : [공지사항]",num_max,":",title)
+            self.noti = "변경사항있음 : [공지사항]",num_max,":",title
+            send_thread = threading.Thread(target = self.send_noti, args = (self.depart, self.noti))
+            self.send_thread_list.append(send_thread)
+            send_thread.start()
+
+            print(self.noti)
             pre_max = num_max
 
         else:                      #키워드리스트 설정했을때
@@ -80,13 +94,25 @@ class depart_noti :
             for keyword in setting.keyword :
                 if keyword in title:    #키워드하나가 포함됨
                     include_keyword = 1
-                    print("변경사항있음 : [공지사항]",num_max,":",title,", 키워드 : ",keyword)
+
+                    self.noti = "변경사항있음 : [공지사항]",num_max,":",title,", 키워드 : ",keyword
+                    send_thread = threading.Thread(target = self.send_noti, args = (self.depart, self.noti))
+                    self.send_thread_list.append(send_thread)
+                    send_thread.start()
+
+                    print(self.noti)
                     break
 
             if include_keyword == 0: #테스트 전용, 원래는 키워드 포함되어있지않으면, 아무것도안함
                 print("변경사항있지만, 키워드포함 x : [공지사항]",num_max,":",title) 
 
+    def send_noti(self,depart,noti): #윈도우10 알림창에 공지사항 알림 보냄
+        toaster = ToastNotifier()
+        title = depart + ' 홈페이지'
+        toaster.show_toast(title,noti,icon_path = None, duration = 3600, threaded = False) #3600초 알림지속
+        
+
 #테스트용
-#if __name__ == '__main__':
-#    noti = depart_noti()
-#    noti.get_change() 
+if __name__ == '__main__':
+    noti = depart_noti("컴퓨터학부")
+    noti.get_change() 
