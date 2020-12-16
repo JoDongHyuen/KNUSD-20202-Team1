27b2fe4c2e480c347d250c3f617fd14543bb51dc
KIMTHE <kcms2369@naver.com>
2020-12-16 16:22:19 +0900

    .

--- Design/gui_lms.py
+++ Design/gui_lms.py
@@ -1,8 +1,11 @@
 import sys
 import json
+import re
 import requests
+from bs4 import BeautifulSoup as bs
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+
 
 LOGIN_INFO = {
     'usr_id': '',
@@ -73,6 +76,7 @@ class basicWindow(QWidget):
         label2.setAlignment(Qt.AlignCenter)
         
         button1 = QPushButton('On/Off')
+        button1.clicked.connect(self.lms_notify)
         grid_layout.addWidget(button1, 1, 0)
         
         button2 = QPushButton('Setting')
@@ -93,6 +97,43 @@ class basicWindow(QWidget):
     # 버튼 이벤트 함수
     def pushButtontoLogin(self):
         self.lms_login.show()
+
+    def lms_notify(self):
+        with requests.session() as s:
+        
+            login_check = 0
+
+            #print(LOGIN_INFO)
+
+            #로그인에 실패하면 login에 isError 가 True로 나타나서 이걸 이용해 exception 처리 예정
+            login_req = s.post('https://lms.knu.ac.kr/ilos/lo/login.acl', data=LOGIN_INFO)
+            login = login_req.text
+            #print(login)
+
+            #로그인 후 알림 창을 스크래핑하는 부분
+            notification = s.get('https://lms.knu.ac.kr/ilos/mp/notification_list.acl')
+            noti = bs(notification.content, 'html.parser')
+            for i,j,k in zip(noti.find_all(class_="notification_subject"),noti.find_all(class_="notification_text"), noti.find_all(class_="notification_day")):
+                i = re.sub('<.+?>', '', str(i), 0).strip()
+                i = i.replace('\n', '')
+                i = i.replace('\t', '')
+                print(i)
+
+                j = re.sub('<.+?>', '', str(j), 0).strip()
+                j = j.replace('\n', '')
+                j = j.replace('\t', '')
+                j = j.replace('\r', '')
+                title = j.split(' ')
+                new = ' '.join(title[1:])
+                new = new.lstrip()
+                tit = title[0]
+                print(tit+'\n'+new)
+
+                k = re.sub('<.+?>', '', str(k), 0).strip()
+                k = k.replace('\n', '')
+                k = k.replace('\t', '')
+                print(k)
+                print('\n')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- Design/lms_crawling.py
+++ Design/lms_crawling.py
@@ -8,42 +8,39 @@ LOGIN_INFO = {
     'usr_id': '',
     'usr_pwd': ''
 }
+class lms_notify:
+    with requests.session() as s:
+        
+        login_check = 0
 
-with requests.session() as s:
-    
-    login_check = 0
+        #print(LOGIN_INFO)
 
-    print(LOGIN_INFO)
+        #로그인에 실패하면 login에 isError 가 True로 나타나서 이걸 이용해 exception 처리 예정
+        login_req = s.post('https://lms.knu.ac.kr/ilos/lo/login.acl', data=LOGIN_INFO)
+        login = login_req.text
+        #print(login)
 
-    #로그인에 실패하면 login에 isError 가 True로 나타나서 이걸 이용해 exception 처리 예정
-    login_req = s.post('https://lms.knu.ac.kr/ilos/lo/login.acl', data=LOGIN_INFO)
-    login = login_req.text
-    print(login)
+        #로그인 후 알림 창을 스크래핑하는 부분
+        notification = s.get('https://lms.knu.ac.kr/ilos/mp/notification_list.acl')
+        noti = bs(notification.content, 'html.parser')
+        for i,j,k in zip(noti.find_all(class_="notification_subject"),noti.find_all(class_="notification_text"), noti.find_all(class_="notification_day")):
+            i = re.sub('<.+?>', '', str(i), 0).strip()
+            i = i.replace('\n', '')
+            i = i.replace('\t', '')
+            print(i)
 
+            j = re.sub('<.+?>', '', str(j), 0).strip()
+            j = j.replace('\n', '')
+            j = j.replace('\t', '')
+            j = j.replace('\r', '')
+            title = j.split(' ')
+            new = ' '.join(title[1:])
+            new = new.lstrip()
+            tit = title[0]
+            print(tit+'\n'+new)
 
-    alarm = s.get('https://lms.knu.ac.kr/ilos/main/main_form.acl')
-    html = alarm.text
-    soup = bs(html,'html.parser')
-    
-
-    #만약 정상적으로 로그인 되었다면 html.txt 파일에 본인 이름을 찾을 수 있음
-    score_file = open("html.txt", "w", encoding="utf8")
-    print(html, file = score_file)
-    score_file.close()
-
-
-    pkg_list = soup.select('div[class=site-map-box]')
-    print(pkg_list)
-    
-
-    #로그인 후 알림 창을 스크래핑하는 부분
-    notification = s.get('https://lms.knu.ac.kr/ilos/mp/notification_list.acl')
-    notification_html = notification.text
-    soup = bs(notification_html, 'html.parser')
-
-    score_file = open("notfi.txt","w",encoding="utf8")
-    
-    notification_html = re.sub('<.+?>', '', notification_html, 0).strip()
-    print(notification_html)
-    print(notification_html, file = score_file)
-    score_file.close()
+            k = re.sub('<.+?>', '', str(k), 0).strip()
+            k = k.replace('\n', '')
+            k = k.replace('\t', '')
+            print(k)
+            print('\n')
